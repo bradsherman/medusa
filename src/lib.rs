cbf90a6e0e5e0e64e7d1bcd76dbceb26c36f4790
Brad Sherman <bsherman1096@gmail.com>
2019-12-12 22:04:48 -0600

    Format code with clippy
    - Keep the code consistent by using clippy to
      automatically format all files

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 extern crate reqwest;
 
 use std::error::Error;
+use std::fmt;
+use std::ops::Div;
+use std::sync::Arc;
 use std::thread;
 use std::time::SystemTime;
-use std::sync::Arc;
-use std::ops::Div;
-use std::fmt;
 
 pub struct Config {
     pub num_threads: u32,
@@ -24,14 +24,18 @@ impl Config {
                     Err(_) => return Err("Didn't get a valid number for number of threads"),
                 };
                 arg
-            },
-            None => return Err("Didn't get a number of threads")
+            }
+            None => return Err("Didn't get a number of threads"),
         };
         let url = match args.next() {
             Some(url) => url,
-            None => return Err("Didn't get a url")
+            None => return Err("Didn't get a url"),
         };
-        Ok(Config { num_threads, url, max_concurrent_requests: None })
+        Ok(Config {
+            num_threads,
+            url,
+            max_concurrent_requests: None,
+        })
     }
 }
 
@@ -53,7 +57,10 @@ impl fmt::Display for Stats {
     }
 }
 
-fn median<T>(mut times: Vec<T>) -> T where T : Div + Ord + Copy {
+fn median<T>(mut times: Vec<T>) -> T
+where
+    T: Div + Ord + Copy,
+{
     times.sort();
     let mid_idx = times.len() / 2;
     times[mid_idx]
@@ -80,13 +87,16 @@ fn calc_stats(threads: Vec<thread::JoinHandle<Result<u128, String>>>) -> Stats {
                 times.push(time);
             }
             Err(e) => {
-                println!("Not adding request stats due to error during request: {}", e);
+                println!(
+                    "Not adding request stats due to error during request: {}",
+                    e
+                );
             }
         }
     }
     let avg = sum / idx as u128;
     let median_time = median(times);
-    Stats{
+    Stats {
         avg_time: avg,
         median_time,
         min_time,
@@ -97,7 +107,10 @@ fn calc_stats(threads: Vec<thread::JoinHandle<Result<u128, String>>>) -> Stats {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut all_threads = Vec::with_capacity(config.num_threads as usize);
-    println!("Load testing '{}' with {} concurrent requests", config.url, config.num_threads);
+    println!(
+        "Load testing '{}' with {} concurrent requests",
+        config.url, config.num_threads
+    );
 
     // Atomic shared pointer to url
     let url = Arc::new(config.url);
@@ -109,16 +122,14 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         let thread = thread::spawn(move || {
             let now = SystemTime::now();
             match reqwest::get(&*url) {
-                Ok(_) => {
-                    match now.elapsed() {
-                        Ok(elapsed) => {
-                            return Ok(elapsed.as_millis());
-                        }
-                        Err(e) => {
-                            return Err(String::from(e.description()));
-                        }
+                Ok(_) => match now.elapsed() {
+                    Ok(elapsed) => {
+                        return Ok(elapsed.as_millis());
                     }
-                }
+                    Err(e) => {
+                        return Err(String::from(e.description()));
+                    }
+                },
                 Err(e) => {
                     return Err(String::from(e.description()));
                 }
