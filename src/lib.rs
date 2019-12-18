79d6f465bb9a1b28bd71337f380ddab89590c60a
Brad Sherman <bsherman1096@gmail.com>
2019-12-18 00:29:35 -0600

    Read Config file
    - Use JSON config file to specify configuration (sample under config.json)
    
    - Do not panic if args are incorrect, but exit gracefully with error

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate reqwest;
 
+use clap::value_t;
+use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::fmt;
 use std::ops::Div;
@@ -8,6 +10,10 @@ use std::thread;
 use std::time;
 use std::time::SystemTime;
 
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Serialize, Deserialize)]
 pub struct Config {
     pub num_threads: u32,
     pub url: String,
@@ -20,6 +26,57 @@ impl Config {
             url: url.to_string(),
             num_threads,
             max_concurrent_requests,
+        }
+    }
+
+    pub fn try_parse(args: clap::ArgMatches) -> Result<Config, String> {
+        /*
+            If a config file is passed in it trumps all
+            Otherwise we will try to parse the given arguments individually
+        */
+        match args.value_of("config") {
+            Some(config_file) => {
+                let mut config_str = String::new();
+                if File::open(config_file)
+                    .map(|mut f| f.read_to_string(&mut config_str))
+                    .is_err()
+                {
+                    Err(format!("Invalid config file path {}", config_file))
+                } else {
+                    serde_json::from_str(&config_str).map_err(|e| {
+                        format!("Invalid json in configuration file {}", e.description())
+                    })
+                }
+            }
+            None => {
+                let url_opt = args.value_of("url");
+                if url_opt.is_none() {
+                    return Err(
+                        "No config file found and url not passed in as an argument".to_string()
+                    );
+                }
+                let url = url_opt.unwrap();
+
+                let thread_opt = args.value_of("threads");
+                if thread_opt.is_none() {
+                    return Err(
+                        "No config file found and threads not passed in as an argument".to_string(),
+                    );
+                }
+                let parse_opt = thread_opt.unwrap().parse::<u32>();
+                if parse_opt.is_err() {
+                    return Err(format!(
+                        "Invalid argument passed in for threads: {}",
+                        thread_opt.unwrap()
+                    ));
+                }
+                let threads = parse_opt.unwrap();
+
+                let max_concurrent_requests =
+                    value_t!(args.value_of("max_concurrent_requests"), u32).ok();
+
+                Ok(Config::new(url, threads, max_concurrent_requests))
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate clap;
+extern crate serde;
+extern crate serde_json;
 
-use clap::{value_t, App, Arg};
+use clap::{App, Arg};
 use std::process;
 
 use medusa::Config;
@@ -11,10 +13,17 @@ fn main() {
         .author("Brad Sherman & Carter Green")
         .about("API load testing tool")
         .arg(
+            Arg::with_name("config")
+                .short("-c")
+                .long("config")
+                .value_name("FILE")
+                .help("JSON file containing the configuration to run")
+                .takes_value(true),
+        )
+        .arg(
             Arg::with_name("url")
                 .short("-u")
                 .long("url")
-                .required(true)
                 .value_name("URL")
                 .help("Sets the url to be tested")
                 .takes_value(true),
@@ -23,7 +32,6 @@ fn main() {
             Arg::with_name("threads")
                 .short("-t")
                 .long("threads")
-                .required(true)
                 .value_name("THREADS")
                 .help("Sets the number of threads to be used")
                 .takes_value(true),
@@ -38,10 +46,10 @@ fn main() {
         )
         .get_matches();
 
-    let url = matches.value_of("url").unwrap();
-    let threads = value_t!(matches.value_of("threads"), u32).unwrap_or_else(|e| e.exit());
-    let max_concurrent_requests = value_t!(matches.value_of("max_concurrent_requests"), u32).ok();
-    let config = Config::new(url, threads, max_concurrent_requests);
+    let config = Config::try_parse(matches).unwrap_or_else(|e| {
+        eprintln!("Error parsing config: {}", e);
+        process::exit(1)
+    });
 
     if let Err(e) = medusa::run(config) {
         eprintln!("Application error: {}", e);
