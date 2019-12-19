ae2e467f7e0d5402cb65973a209e1a1a5e968393
Carter Green <cartergr@usc.edu>
2019-12-18 21:15:18 -0600

    Pattern matching and combinators

--- src/config.rs
+++ src/config.rs
@@ -22,52 +22,40 @@ impl Config {
 
     pub fn try_parse(args: clap::ArgMatches) -> Result<Config, String> {
         /*
-            If a config file is passed in it trumps all
-            Otherwise we will try to parse the given arguments individually
+            If a config file is passed in it trumps all other arguments,
+            otherwise we will try to parse the given arguments individually
         */
         match args.value_of("config") {
-            Some(config_file) => {
-                let mut config_str = String::new();
-                if File::open(config_file)
-                    .map(|mut f| f.read_to_string(&mut config_str))
-                    .is_err()
-                {
-                    Err(format!("Invalid config file path {}", config_file))
-                } else {
-                    serde_json::from_str(&config_str).map_err(|e| {
-                        format!("Invalid json in configuration file {}", e.description())
-                    })
-                }
-            }
-            None => {
-                let url_opt = args.value_of("url");
-                if url_opt.is_none() {
-                    return Err(
-                        "No config file found and url not passed in as an argument".to_string()
-                    );
-                }
-                let url = url_opt.unwrap();
+            Some(config_file) => Config::try_parse_file(&config_file),
+            None => Config::try_parse_args(args),
+        }
+    }
 
-                let thread_opt = args.value_of("threads");
-                if thread_opt.is_none() {
-                    return Err(
-                        "No config file found and threads not passed in as an argument".to_string(),
-                    );
-                }
-                let parse_opt = thread_opt.unwrap().parse::<u32>();
-                if parse_opt.is_err() {
-                    return Err(format!(
-                        "Invalid argument passed in for threads: {}",
-                        thread_opt.unwrap()
-                    ));
-                }
-                let threads = parse_opt.unwrap();
+    fn try_parse_file(config_file: &str) -> Result<Config, String> {
+        let mut config_str = String::new();
+        File::open(config_file)
+            .map(|mut f| f.read_to_string(&mut config_str))
+            .map_err(|e| format!("Unable to open file: {}", e.description()))
+            .and_then(|_| serde_json::from_str(&config_str).map_err(|e| {
+                format!("Invalid json in configuration file {}", e.description())
+            }))
+    }
 
-                let max_concurrent_requests =
-                    value_t!(args.value_of("max_concurrent_requests"), u32).ok();
-
-                Ok(Config::new(url, threads, max_concurrent_requests))
-            }
+    fn try_parse_args(args: clap::ArgMatches) -> Result<Config, String> {
+        match (args.value_of("url"), args.value_of("threads")) {
+            (Some(url), Some(num_threads)) => {
+                match num_threads.parse::<u32>() {
+                    Ok(num_threads) if num_threads > 0 => {
+                        let max_concurrent_requests =
+                            value_t!(args.value_of("max_concurrent_requests"), u32).ok();
+                        Ok(Config::new(url, num_threads, max_concurrent_requests))
+                    },
+                    _ => Err("`threads` should be a positive integer".to_owned())
+                }
+            },
+            (None, Some(_)) => Err("Missing required argument `url`".to_owned()),
+            (Some(_), None) => Err("Missing required argument `threads`".to_owned()),
+            (None, None) => Err("Missing required arguments `url` and `threads`".to_owned()),
         }
     }
 }
